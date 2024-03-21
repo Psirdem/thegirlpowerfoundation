7581b92623bd76191f289b8ae315fe3e011bad27
Kafui Mortey <morteykafui@gmail.com>
2024-03-21 19:19:26 +0000

    add map cordinates

--- components/Map.tsx
+++ components/Map.tsx
@@ -12,9 +12,9 @@ const Map = () => {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
       />
-      <Marker position={[6.67508, -1.61645]}>
+      <Marker position={[6.66757458115214, -1.5265636758437102]}>
         <Popup>
-          <span>Our location</span>
+          <span>Appiadu, Kumasi</span>
         </Popup>
       </Marker>
     </MapContainer>
